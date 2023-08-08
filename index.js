437692dd4e523eaa7172a61b1f33f2507b5c2a0c
Wissididom <30803034+Wissididom@users.noreply.github.com>
2023-08-09 00:19:12 +0200

    Add Prettier Github Action (#27)

--- index.js
+++ index.js
@@ -1,33 +1,29 @@
-import * as dotenv from 'dotenv';
-import {
-	Client,
-	GatewayIntentBits,
-	Partials
-} from 'discord.js';
-import {
-	DateTime
-} from 'luxon';
+import * as dotenv from "dotenv";
+import { Client, GatewayIntentBits, Partials } from "discord.js";
+import { DateTime } from "luxon";
 
 dotenv.config();
 
+const SUPPORTED_TIMEZONES = Intl.supportedValuesOf("timeZone");
+
 const client = new Client({
-	intents: [
-		GatewayIntentBits.Guilds,
-		GatewayIntentBits.GuildMessages,
-		GatewayIntentBits.DirectMessages
-	],
-	partials: [
-		Partials.User,
-		Partials.Channel,
-		Partials.GuildMember,
-		Partials.Message,
-		Partials.Reaction
-	]
+  intents: [
+    GatewayIntentBits.Guilds,
+    GatewayIntentBits.GuildMessages,
+    GatewayIntentBits.DirectMessages,
+  ],
+  partials: [
+    Partials.User,
+    Partials.Channel,
+    Partials.GuildMember,
+    Partials.Message,
+    Partials.Reaction,
+  ],
 });
 
 function getContent(unix, preferUsability = false) {
-	if (preferUsability)
-		return `
+  if (preferUsability)
+    return `
 <t:${unix}>: \`<t:${unix}>\`
 <t:${unix}:t>: \`<t:${unix}:t>\`
 <t:${unix}:T>: \`<t:${unix}:T>\`
@@ -37,8 +33,8 @@ function getContent(unix, preferUsability = false) {
 <t:${unix}:F>: \`<t:${unix}:F>\`
 <t:${unix}:R>: \`<t:${unix}:R>\`
 	`;
-	else
-		return `
+  else
+    return `
 \`<t:${unix}>\`: <t:${unix}>
 \`<t:${unix}:t>\`: <t:${unix}:t>
 \`<t:${unix}:T>\`: <t:${unix}:T>
@@ -50,59 +46,95 @@ function getContent(unix, preferUsability = false) {
 	`;
 }
 
-function getConsoleContent(currenttimestamp, ephemeral, day, month, year, hour, minute, second, timezone) {
-	if (currenttimestamp)
-		return `[currenttimestamp] Executed /currenttimestamp (Ephemeral: ${ephemeral})`;
-	return `[timestamp] Day: ${day}; Month: ${month}; Year: ${year}; Hour: ${hour}; Minute: ${minute}; Second: ${second}; Timezone: ${timezone}; Ephemeral: ${ephemeral}`;
+function getConsoleContent(
+  currenttimestamp,
+  ephemeral,
+  day,
+  month,
+  year,
+  hour,
+  minute,
+  second,
+  timezone,
+) {
+  if (currenttimestamp)
+    return `[currenttimestamp] Executed /currenttimestamp (Ephemeral: ${ephemeral})`;
+  return `[timestamp] Day: ${day}; Month: ${month}; Year: ${year}; Hour: ${hour}; Minute: ${minute}; Second: ${second}; Timezone: ${timezone}; Ephemeral: ${ephemeral}`;
 }
 
 client.on("ready", () => {
-	console.log(`Logged in as ${client.user.tag}!`); // Logging
+  console.log(`Logged in as ${client.user.tag}!`); // Logging
 });
 
-client.on("interactionCreate", async interaction => {
-	if (interaction.isCommand()) {
-		let preferUsability = interaction.options.getBoolean('prefer_usability') ?? false;
-		let ephemeral = !interaction.options.getBoolean('public');
-		let unix = 0;
-		switch (interaction.commandName) {
-			case 'timestamp':
-				await interaction.deferReply({ ephemeral });
-				let day = interaction.options.getInteger('day') ?? 1;
-				let month = interaction.options.getInteger('month') ?? 1;
-				let year = interaction.options.getInteger('year') ?? 1970;
-				let hour = interaction.options.getInteger('hour') ?? 0;
-				let minute = interaction.options.getInteger('minute') ?? 0;
-				let second = interaction.options.getInteger('second') ?? 0;
-				let timezone = interaction.options.getString('timezone');
-				//Intl.DateTimeFormat().resolvedOptions().timeZone // own timezone
-				unix = DateTime.now().setZone(timezone).set({ day, month, year, hour, minute, second}).toUnixInteger();
-				await interaction.editReply({
-					content: getContent(unix, preferUsability)
-				});
-				console.log(getConsoleContent(false, ephemeral, day, month, year, hour, minute, second, timezone));
-				break;
-			case 'currenttimestamp':
-				await interaction.deferReply({ ephemeral });
-				unix = DateTime.now().toUnixInteger();
-				await interaction.editReply({
-					content: getContent(unix, preferUsability)
-				});
-				console.log(getConsoleContent(true, ephemeral));
-				break;
-		}
-	} else if (interaction.isAutocomplete()) {
-		let timezoneResponse = Intl.supportedValuesOf('timeZone').filter(zone => {
-			return zone.toLowerCase().indexOf(interaction.options.getFocused().replace(' ', '_').toLowerCase()) >= 0;
-		});
-		timezoneResponse.length = Math.min(timezoneResponse.length, 25); // send max. 25 choices
-		await interaction.respond(timezoneResponse.map(zone => {
-			return {
-				name: zone,
-				value: zone
-			};
-		})).catch(err => console.error(JSON.stringify(err)));
-	}
+client.on("interactionCreate", async (interaction) => {
+  if (interaction.isCommand()) {
+    let preferUsability =
+      interaction.options.getBoolean("prefer_usability") ?? false;
+    let ephemeral = !interaction.options.getBoolean("public");
+    let unix = 0;
+    switch (interaction.commandName) {
+      case "timestamp":
+        await interaction.deferReply({ ephemeral });
+        let day = interaction.options.getInteger("day") ?? 1;
+        let month = interaction.options.getInteger("month") ?? 1;
+        let year = interaction.options.getInteger("year") ?? 1970;
+        let hour = interaction.options.getInteger("hour") ?? 0;
+        let minute = interaction.options.getInteger("minute") ?? 0;
+        let second = interaction.options.getInteger("second") ?? 0;
+        let timezone = interaction.options.getString("timezone");
+        //Intl.DateTimeFormat().resolvedOptions().timeZone // own timezone
+        unix = DateTime.now()
+          .setZone(timezone)
+          .set({ day, month, year, hour, minute, second })
+          .toUnixInteger();
+        await interaction.editReply({
+          content: getContent(unix, preferUsability),
+        });
+        console.log(
+          getConsoleContent(
+            false,
+            ephemeral,
+            day,
+            month,
+            year,
+            hour,
+            minute,
+            second,
+            timezone,
+          ),
+        );
+        break;
+      case "currenttimestamp":
+        await interaction.deferReply({ ephemeral });
+        unix = DateTime.now().toUnixInteger();
+        await interaction.editReply({
+          content: getContent(unix, preferUsability),
+        });
+        console.log(getConsoleContent(true, ephemeral));
+        break;
+    }
+  } else if (interaction.isAutocomplete()) {
+    let timezoneResponse = SUPPORTED_TIMEZONES.filter((zone) => {
+      return (
+        zone
+          .toLowerCase()
+          .indexOf(
+            interaction.options.getFocused().replace(" ", "_").toLowerCase(),
+          ) >= 0
+      );
+    });
+    timezoneResponse.length = Math.min(timezoneResponse.length, 25); // send max. 25 choices
+    await interaction
+      .respond(
+        timezoneResponse.map((zone) => {
+          return {
+            name: zone,
+            value: zone,
+          };
+        }),
+      )
+      .catch((err) => console.error(JSON.stringify(err)));
+  }
 });
 
-client.login(process.env['TOKEN']);
+client.login(process.env["TOKEN"]);
--- setupCommands.js
+++ setupCommands.js
@@ -1,120 +1,127 @@
-import * as dotenv from 'dotenv';
+import * as dotenv from "dotenv";
 import {
-	Client,
-	GatewayIntentBits,
-	Partials,
-	ApplicationCommandType,
-	ApplicationCommandOptionType
-} from 'discord.js';
+  Client,
+  GatewayIntentBits,
+  Partials,
+  ApplicationCommandType,
+  ApplicationCommandOptionType,
+} from "discord.js";
 
 dotenv.config();
 
 const client = new Client({
-	intents: [
-		GatewayIntentBits.Guilds,
-		GatewayIntentBits.GuildMessages,
-		GatewayIntentBits.DirectMessages
-	],
-	partials: [
-		Partials.User,
-		Partials.Channel,
-		Partials.GuildMember,
-		Partials.Message,
-		Partials.Reaction
-	]
+  intents: [
+    GatewayIntentBits.Guilds,
+    GatewayIntentBits.GuildMessages,
+    GatewayIntentBits.DirectMessages,
+  ],
+  partials: [
+    Partials.User,
+    Partials.Channel,
+    Partials.GuildMember,
+    Partials.Message,
+    Partials.Reaction,
+  ],
 }); // Discord Object
 
 client.on("ready", () => {
-	console.log(`Logged in as ${client.user.tag}!`); // Logging
-	const registerObject = {
-		timestamp: {
-			name: 'timestamp',
-			description: 'Generate Discord timestamps based on a given time and timezone',
-			type: ApplicationCommandType.ChatInput,
-			options: [
-				{
-					name: 'day',
-					description: 'Day (1 - 31)',
-					required: true,
-					type: ApplicationCommandOptionType.Integer
-				},
-				{
-					name: 'month',
-					description: 'Month (1 - 12)',
-					required: true,
-					type: ApplicationCommandOptionType.Integer
-				},
-				{
-					name: 'year',
-					description: 'Year (min. 1970)',
-					required: true,
-					type: ApplicationCommandOptionType.Integer
-				},
-				{
-					name: 'hour',
-					description: 'Hour (0 - 23)',
-					required: true,
-					type: ApplicationCommandOptionType.Integer
-				},
-				{
-					name: 'minute',
-					description: 'Minute (0 - 59)',
-					required: true,
-					type: ApplicationCommandOptionType.Integer
-				},
-				{
-					name: 'timezone',
-					description: 'Timezone (Format: continent/city)',
-					required: true,
-					type: ApplicationCommandOptionType.String,
-					autocomplete: true
-				},
-				{
-					name: 'second',
-					description: 'Second (0 - 59)',
-					required: false,
-					type: ApplicationCommandOptionType.Integer
-				},
-				{
-					name: 'prefer_usability',
-					description: '"preview: code" (true); "code: preview" (false)',
-					required: false,
-					type: ApplicationCommandOptionType.Boolean
-				},
-				{
-					name: 'public',
-					description: 'Should the response be visible to everyone? (Default: False)',
-					required: false,
-					type: ApplicationCommandOptionType.Boolean
-				}
-			]
-		},
-		currenttimestamp: {
-			name: 'currenttimestamp',
-			description: 'Generate Discord timestamps based on the current time',
-			type: ApplicationCommandType.ChatInput,
-			options: [
-				{
-					name: 'prefer_usability',
-					description: '"preview: code" (true); "code: preview" (false)',
-					required: false,
-					type: ApplicationCommandOptionType.Boolean
-				},
-				{
-					name: 'public',
-					description: 'Should the response be visible to everyone? (Default: False)',
-					required: false,
-					type: ApplicationCommandOptionType.Boolean
-				}
-			]
-		}
-	};
-	let promises = [];
-	promises.push(client.application?.commands?.create(registerObject['timestamp']));
-	promises.push(client.application?.commands?.create(registerObject['currenttimestamp']));
-	Promise.all(promises).then(reolvedPromises => {
-		process.kill(process.pid, 'SIGTERM');
-	});
+  console.log(`Logged in as ${client.user.tag}!`); // Logging
+  const registerObject = {
+    timestamp: {
+      name: "timestamp",
+      description:
+        "Generate Discord timestamps based on a given time and timezone",
+      type: ApplicationCommandType.ChatInput,
+      options: [
+        {
+          name: "day",
+          description: "Day (1 - 31)",
+          required: true,
+          type: ApplicationCommandOptionType.Integer,
+        },
+        {
+          name: "month",
+          description: "Month (1 - 12)",
+          required: true,
+          type: ApplicationCommandOptionType.Integer,
+        },
+        {
+          name: "year",
+          description: "Year (min. 1970)",
+          required: true,
+          type: ApplicationCommandOptionType.Integer,
+        },
+        {
+          name: "hour",
+          description: "Hour (0 - 23)",
+          required: true,
+          type: ApplicationCommandOptionType.Integer,
+        },
+        {
+          name: "minute",
+          description: "Minute (0 - 59)",
+          required: true,
+          type: ApplicationCommandOptionType.Integer,
+        },
+        {
+          name: "timezone",
+          description: "Timezone (Format: continent/city)",
+          required: true,
+          type: ApplicationCommandOptionType.String,
+          autocomplete: true,
+        },
+        {
+          name: "second",
+          description: "Second (0 - 59)",
+          required: false,
+          type: ApplicationCommandOptionType.Integer,
+        },
+        {
+          name: "prefer_usability",
+          description: '"preview: code" (true); "code: preview" (false)',
+          required: false,
+          type: ApplicationCommandOptionType.Boolean,
+        },
+        {
+          name: "public",
+          description:
+            "Should the response be visible to everyone? (Default: False)",
+          required: false,
+          type: ApplicationCommandOptionType.Boolean,
+        },
+      ],
+    },
+    currenttimestamp: {
+      name: "currenttimestamp",
+      description: "Generate Discord timestamps based on the current time",
+      type: ApplicationCommandType.ChatInput,
+      options: [
+        {
+          name: "prefer_usability",
+          description: '"preview: code" (true); "code: preview" (false)',
+          required: false,
+          type: ApplicationCommandOptionType.Boolean,
+        },
+        {
+          name: "public",
+          description:
+            "Should the response be visible to everyone? (Default: False)",
+          required: false,
+          type: ApplicationCommandOptionType.Boolean,
+        },
+      ],
+    },
+  };
+  let promises = [];
+  promises.push(
+    client.application?.commands?.create(registerObject["timestamp"]),
+  );
+  promises.push(
+    client.application?.commands?.create(registerObject["currenttimestamp"]),
+  );
+  Promise.all(promises).then((reolvedPromises) => {
+    process.kill(process.pid, "SIGTERM");
+  });
 });
 
-client.login(process.env['TOKEN']);
+client.login(process.env["TOKEN"]);
